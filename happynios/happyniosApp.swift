5fd89b2400f9b54779ac7ab2c546e48806f7183f
happyntec <tec@happyn.cn>
2023-04-10 18:30:21 +0800

    for #1

---
+++ happynios/happyniosApp.swift
@@ -0,0 +1,17 @@
+//
+//  happyniosApp.swift
+//  happynios
+//
+//  Created by mac on 2023/4/9.
+//
+
+import SwiftUI
+
+@main
+struct happyniosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
